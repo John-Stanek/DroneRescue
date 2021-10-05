c2477ee4fd53e8999fc25acd3cf941367810e5b1
jakeziegelbein <ziege090@umn.edu>
2021-10-05 01:40:00 +0000

    Adding lab04 deliverables

--- labs/lab04_polymorphism/tree.h
+++ labs/lab04_polymorphism/tree.h
@@ -1,6 +1,7 @@
 #ifndef TREE_H_
 #define TREE_H_
 
+#include<vector>
 #include "entity.h"
 
 // Represents a Tree in a physical system
@@ -8,19 +9,17 @@ class Tree : public Entity {
 public:
     // Trees are on the ground, so do not have a z component
     Tree(const std::string& name, double x, double y) : name(name) {
-        pos[0] = x;
-        pos[1] = y;
+        pos.push_back(x);
+        pos.push_back(y);
     }
     // Gets the tree name
     const std::string& GetName() { return name; }
     // Gets the tree position
-    double* GetPosition() { return pos; }
+    std::vector<double> GetPosition() { return pos; }
 
 private:
-    double* pos;
-    const std::string& name;
+    std::vector<double> pos;
+    std::string name;
 };
-
-
 
 #endif
