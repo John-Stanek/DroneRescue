444b486fec2a91d22c24ea54340774b9af51cc88
Jake Ziegelbein <ziege090@umn.edu>
2021-10-22 16:45:32 -0500

    improving filter

--- project/double_threshold_filter.cc
+++ project/double_threshold_filter.cc
@@ -7,22 +7,40 @@ DoubleThresholdFilter::DoubleThresholdFilter(float strong, float weak) {
 
 void DoubleThresholdFilter::Apply(std::vector<Image*> original, std::vector<Image*> filtered) {
     *filtered[0] = *original[0];
-    float highThreshold = 1.0*strong_threshold_ratio;
-    float weakThreshold = 1.0*weak_threshold_ratio;
     float strong = 1.0;
     float weak = 0.25;
     float none = 0.0;
+    float max = 0.0;
 
     for(int x=0; x<filtered[0]->GetWidth(); x++) {
         for(int y=0; y<filtered[0]->GetHeight(); y++) {
             Color pixel = filtered[0]->GetPixel(x, y);
-            if(pixel.Red() < highThreshold && pixel.Red() >= weakThreshold) {
-                pixel = weak;
-                filtered[0]->SetPixel(x, y, pixel);                
-            } else if(pixel.Red() > highThreshold) {
+            if(pixel.Red() >= max) {
+              max = pixel.Red();
+            }
+            if(pixel.Green() >= max) {
+              max = pixel.Green();
+            }
+            if(pixel.Blue() >= max) {
+              max = pixel.Blue();
+            }
+        }
+    }
+
+    float highThreshold = strong_threshold_ratio*max;
+    float lowThreshold = weak_threshold_ratio*highThreshold;
+  
+    for(int x=0; x<filtered[0]->GetWidth(); x++) {
+        for(int y=0; y<filtered[0]->GetHeight(); y++) {
+            Color pixel = filtered[0]->GetPixel(x, y);
+            if(pixel.Red() >= highThreshold){
                 pixel = strong;
                 filtered[0]->SetPixel(x, y, pixel);
-            } else {
+            }
+            else if(pixel.Red() <= highThreshold && pixel.Red() >= lowThreshold) {
+                pixel = weak;
+                filtered[0]->SetPixel(x, y, pixel);                
+            } else if(pixel.Red() < lowThreshold){
                 pixel = none;
                 filtered[0]->SetPixel(x, y, pixel);
             } 
