6572e376b98a653552a9121025a619d15982717d
Jake Ziegelbein <ziege090@umn.edu>
2021-10-22 18:16:56 -0500

    Improved filter

--- project/hysteresis_filter.cc
+++ project/hysteresis_filter.cc
@@ -2,24 +2,25 @@
 
 void HysteresisFilter::Apply(std::vector<Image*> original, std::vector<Image*> filtered) {
     *filtered[0] = *original[0];
-    float strong = 255.0;
-    float weak = 25.0;
+    float strong = 1.0;
+    float weak = 0.10;
     float none = 0.0;
 
     for(int x=0; x<filtered[0]->GetWidth(); x++) {
         for(int y=0; y<filtered[0]->GetHeight(); y++) {
             Color pixel = filtered[0]->GetPixel(x, y);
-            if(pixel.Red()*255 == weak) {
-                if(filtered[0]->GetPixel(x, y-1).Red()*255 >= strong || filtered[0]->GetPixel(x, y+1).Red()*255 >= strong ||
-                    filtered[0]->GetPixel(x-1, y-1).Red()*255 >= strong || filtered[0]->GetPixel(x-1, y).Red()*255 >= strong ||
-                    filtered[0]->GetPixel(x-1, y+1).Red()*255 >= strong || filtered[0]->GetPixel(x+1, y-1).Red()*255 >= strong ||
-                    filtered[0]->GetPixel(x+1, y).Red()*255 >= strong || filtered[0]->GetPixel(x+1, y+1).Red()*255 >= strong) {
-                    
-                    pixel = strong;
+            float alpha = pixel.Alpha();
+            if(pixel.Red() <= weak && pixel.Red() >= .09) {
+                if(filtered[0]->GetPixel(x, y-1).Red() == strong || filtered[0]->GetPixel(x, y+1).Red() == strong ||
+                    filtered[0]->GetPixel(x-1, y-1).Red() == strong || filtered[0]->GetPixel(x-1, y).Red() == strong ||
+                    filtered[0]->GetPixel(x-1, y+1).Red() == strong || filtered[0]->GetPixel(x+1, y-1).Red() == strong ||
+                    filtered[0]->GetPixel(x+1, y).Red() == strong || filtered[0]->GetPixel(x+1, y+1).Red() == strong) {
+                  pixel = strong;  
+                } else {
+                  pixel = none;
                 }
-            } else {
-                pixel = none;
             }
+            pixel.SetAlpha(alpha);
             filtered[0]->SetPixel(x, y, pixel);
         }
     }
