63e408e12af913b5937217b1527f4010faecd9e8
Jake Ziegelbein <ziege090@umn.edu>
2021-10-31 15:56:54 +0000

    Add docs/ folder to gitignore

--- project/double_threshold_filter.h
+++ project/double_threshold_filter.h
@@ -3,12 +3,36 @@
 
 #include "filter.h"
 
+/**
+ * @brief The Double Threshold Filter class
+ *
+ *   Inherits from the Filter class.
+ *   This class has no subclasses
+ * 
+ */
+
 class DoubleThresholdFilter : public Filter {
 private:
     float strong_threshold_ratio;
     float weak_threshold_ratio;
 public:
+
+    /**
+     * @brief Construct a new Double Threshold Filter object
+     * @return Returns an object of the type DoubleThresholdFilter.
+     * @param strong - a float value for the strong threshold ratio. 
+     * @param weak - a float value for the weak threshold ratio.
+     */
+
     DoubleThresholdFilter(float strong, float weak);
+
+    /**
+     * @brief Applies the double threshold filter to the vector of Images original
+     * @return Returns a vector of images that were applied the double threshold filter
+     * @param original - a vector of input images
+     * @param filtered - a vector of the filtered input images
+     */
+
     void Apply(std::vector<Image*> original, std::vector<Image*> filtered);
 };
 
---
+++ project/mainpage.h
@@ -0,0 +1,7 @@
+/*! \mainpage Search And Rescue Drone 
+ *
+ * \section intro_sec Search and resuces stuff
+ *
+ * This is the thing.
+ *
+ */
