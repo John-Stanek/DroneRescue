6da64af359dd6f7d57f2bfcbb8f716bd6682e8ed
Jake Ziegelbein <ziege090@umn.edu>
2021-10-31 17:25:55 +0000

    More doxygen

--- project/mean_blur_filter.h
+++ project/mean_blur_filter.h
@@ -4,8 +4,23 @@
 #include <iostream>
 #include "filter.h"
 
+/**
+ * @brief The class for the mean blur filter.
+ *      Used to apply a mean blur filter to an image.
+ *      Inhereits from Filter and has no child classes.
+ * 
+ */
+
 class MeanBlurFilter : public Filter {
 public:
+
+    /**
+     * @brief Overrides the base class method and applies the mean blur filter to an image
+     * 
+     * @param original - a vector of images.
+     * @param filtered - a vector of images with the mean blur filter applied.
+     */
+
     void Apply(std::vector<Image*> original, std::vector<Image*> filtered);
 };
 
