07156df8918d51dd2d75b957edd6d9c35664f1aa
ross0619 <ross0619@umn.edu>
2021-10-31 22:20:38 -0500

    gaussian.h with dyoxygen

--- project/gaussian.h
+++ project/gaussian.h
@@ -4,14 +4,38 @@
 
 class GaussianBlurFilter : public Filter {
     public:
+    /**
+     * @brief Construct a new Gaussian Blur Filter object
+     * 
+     */
     GaussianBlurFilter();
+    /**
+     * @brief Construct a new Gaussian Blur Filter object with a size and sigma
+     * 
+     * @param sigma 
+     * @param size 
+     */
     GaussianBlurFilter(float sigma, int size);
-    //GaussianBlurFilter(const GaussianBlurFilter& sigma);
+    /**
+     * @brief Destroy the Gaussian Blur object
+     * 
+     */
+
     ~GaussianBlurFilter(){}
+    /**
+     * @brief Appply manipulates the image runs the gaussian blur on the image
+     * 
+     * @param original 
+     * @param filtered 
+     */
     void Apply(std::vector<Image*> original, std::vector<Image*> filtered);
-    //char *Gaussian_Kernel(float size, float signma);
+   
 
     private:
+    /**
+     * @brief Two private variable size and sigma. Size is how large the kernel is.
+     * 
+     */
         float sigma;
         int size;
      
