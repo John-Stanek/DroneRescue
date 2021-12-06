44c998a1be1101472d356ad1d495a874d8501d1b
Aaron Ross <ross0619@umn.edu>
2021-12-05 19:51:37 -0600

    with comments

--- project/src/battery.cc
+++ project/src/battery.cc
@@ -1,21 +1,26 @@
 #include "battery.h"
 
-
+// Creates a battery with a battery life of 5000
 Battery::Battery(){
-    batteryLife = 1010;
+    batteryLife = 5000;
 }
+// Creates a battery with a specific amount of battery
 Battery::Battery(float batLife){
     batteryLife = batLife;
 }
+// Sets the battery life 
 void Battery::SetBatteryLife(float batLife){
     batteryLife = batLife;
 }
+// Gets what the battery life is
 float Battery::GetBatteryLife(){
     return batteryLife;
 }
+// Reduces the battery life by an input
 void Battery::ReduceBatteryLife(float dt){
     batteryLife -= dt;
 }
+// Recharges the battery life by an input
 void Battery::RechargeBatteryLife(float dt){
     batteryLife += dt;
 }
--- project/src/drone.cc
+++ project/src/drone.cc
@@ -59,14 +59,17 @@ void Drone::SetJoystick(double arrows[4], bool moves[2]) {
 void Drone::Update(double dt, double arrows[4], bool moves[2]) {
     this->SetJoystick(arrows, moves);
     double* new_position;
+    // Hard coding the postion of the recharge station
     double rspos[3] = {0, 0, 0};
+    // Reduces the battery life of the drone everytime update is called
     this->battery.ReduceBatteryLife(dt);
-    std::cout<< this->battery.GetBatteryLife() << std::endl;
+    // Check if the battery life is zero and sets a bool final to true and will stop moving the drone
     if(this->battery.GetBatteryLife() <= 0){
         final = true;
     }
+    // Check if the battery is less that 20% and final is false
     if(this->battery.GetBatteryLife() < 1000 && !final){
-
+        // Beelines to where the recharge station is
         this->movement = new Beeline();
         new_position = this->movement->move(pos, rspos, speed);
 
@@ -74,13 +77,15 @@ void Drone::Update(double dt, double arrows[4], bool moves[2]) {
             pos[i] = new_position[i];
         }
         delete this->movement;
-
+        // Checks if the drone position is the same as the recharge station position
         if((int)pos[0] == (int)rspos[0] && (int)pos[1] == (int)rspos[1] && (int)pos[2] == (int)rspos[2]){
-            
+            // Once drone is at the recharge station position it will recharge the drones battery
             Recharge(this);}
 
     }
+    // Check if the battery life is greater that 20% and final is false
     if(this->battery.GetBatteryLife() > 1000 && !final){
+        // Patrols the map while battery life is greater than 20%
         this->movement = new Patrol();
         new_position = this->movement->move(pos, dir, speed);
         for (int i=0; i < 3; i++) {
@@ -88,23 +93,6 @@ void Drone::Update(double dt, double arrows[4], bool moves[2]) {
         }
         delete this->movement;
     }
-    
-    // if (patrol || beeline && !final) {
-    //     if (patrol) { this->movement = new Patrol(); }
-    //     else if (beeline) { this->movement = new Beeline(); }
-    //     new_position = this->movement->move(pos, dir, speed);
-
-    //     for (int i=0; i < 3; i++) {
-    //         pos[i] = new_position[i];
-    //     }
-    //     delete this->movement;
-    // }
-    // else {
-    //     for (int i = 0; i < 3; i++) {
-    //         pos[i] += speed*dir[i]*dt;
-    //         //std::cout << pos[i] << std::endl;
-    //     }
-    // }
 
     // Take a picture every 5 seconds with front camera
     time += dt;
