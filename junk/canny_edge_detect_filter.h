9c47af68fa74c39a425a0d3e5163e3dc226992c7
Jake Ziegelbein <ziege090@umn.edu>
2021-12-11 17:38:27 -0600

    commit to checkout

---
+++ junk/canny_edge_detect_filter.h
@@ -0,0 +1,31 @@
+#ifndef CANNY_FILTER_H
+#define CANNY_FILTER_H
+
+#include "filter.h"
+#include "greyscale_filter.h"
+#include "gaussian.h"
+#include "sobel.h"
+#include "non_max_suppression.h"
+#include "double_threshold_filter.h"
+#include "hysteresis_filter.h"
+
+/**
+ * @brief The Canny Edge Detect Filter class
+ *
+ *   Inherits from the Filter class. Has no subclasses.
+ * 
+ */
+class CannyEdgeDetectFilter : public Filter {
+public:
+
+    /**
+     * @brief Applies the Canny Edge Detect filter to the Image* vector original
+     * @return Adds the filtered images to the Image* vector filtered
+     * @param original - a Image* vector of input images
+     * @param filtered - a Image* vector of the filtered input images
+     */
+    void Apply(std::vector<Image*> original, std::vector<Image*> filtered);
+};
+
+
+#endif
