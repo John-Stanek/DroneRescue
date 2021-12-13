7f7d1ad3f4f866e1035c9f27b5439abcb9b9eb35
Jake Ziegelbein <ziege090@umn.edu>
2021-12-13 17:13:36 -0600

    deleting junk folder and make docker file for deployment

--- project/src/drone.cc
+++ project/src/drone.cc
@@ -6,12 +6,8 @@
 
 Drone::Drone(ICameraController& cameraController) {
     id = 0;
-
     speed = 0;
 
-    // dir[0] = 0;
-    // dir[1] = 0;
-    // dir[2] = 0;
     x=0;
     y=0;
     z=0;
@@ -22,12 +18,8 @@ Drone::Drone(ICameraController& cameraController) {
 
 Drone::Drone() {
     id = 0;
-
     speed = 0;
 
-    // dir[0] = 0;
-    // dir[1] = 0;
-    // dir[2] = 0;
     x=0;
     y=0;
     z=0;
@@ -69,11 +61,6 @@ void Drone::SetJoystick(double arrows[4], bool moves[2]) {
     }
 }
 
-// void Drone::SetJoystick(double x, double y, double z, double a, bool p, bool b) {
-//     patrol = p;
-//     beeline = b;
-//     dir[0]= x; dir[1] = y; dir[2] = z;
-// }
 
 void Drone::Update(double dt, double arrows[4], bool moves[2]) {
     this->SetJoystick(arrows, moves);
@@ -102,7 +89,17 @@ void Drone::Update(double dt, double arrows[4], bool moves[2]) {
             this->battery.Recharge(4000);}
 
     }
-    if(hasCamera){
+    else if(this->battery.GetBatteryLife() > 1000 && !final){
+        // Patrols the map while battery life is greater than 20%
+        this->movement = new Patrol();
+        new_position = this->movement->move(pos, dir, speed);
+        for (int i=0; i < 3; i++) {
+            pos[i] = new_position[i];
+        }
+        delete this->movement;
+    }
+    
+    else if (hasCamera){
         if(camera->result.found) {
             this->movement = new Beeline();
             new_position = this->movement->move(pos, dir, speed);
@@ -118,12 +115,11 @@ void Drone::Update(double dt, double arrows[4], bool moves[2]) {
         else if (beeline) { this->movement = new Beeline(); }
         new_position = this->movement->move(pos, dir, speed);
 
-
-            for (int i=0; i < 3; i++) {
+        for (int i=0; i < 3; i++) {
                 pos[i] = new_position[i];
             }
             delete this->movement;
-        }       
+    }       
     else {
         for (int i = 0; i < 3; i++) {
             pos[i] += speed*dir[i]*dt;
@@ -132,15 +128,6 @@ void Drone::Update(double dt, double arrows[4], bool moves[2]) {
 
     
     // Check if the battery life is greater that 20% and final is false
-    if(this->battery.GetBatteryLife() > 1000 && !final){
-        // Patrols the map while battery life is greater than 20%
-        this->movement = new Patrol();
-        new_position = this->movement->move(pos, dir, speed);
-        for (int i=0; i < 3; i++) {
-            pos[i] = new_position[i];
-        }
-        delete this->movement;
-    }
 
 
     // Take a picture every 5 seconds with front camera
